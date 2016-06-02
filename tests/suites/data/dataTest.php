48fcb1ba1cbd71ec6eef24d900c88575bb01119f
Gino Pane <sergey.karavay@gmail.com>
2016-06-02 15:52:54 +0300

    Update dataTest.
    Updates:
    - Add skeleton logic.

--- tests/suites/data/dataTest.php
+++ tests/suites/data/dataTest.php
@@ -1,13 +1,45 @@
 <?php
 
+use OpenWorld\DataFile;
+
 class DataTest extends PHPUnit_Framework_TestCase
 {
+    protected $dataProvider = null;
+
     protected function setUp()
     {
+        $this->dataProvider = new DataFile();
+
         $this->markTestSkipped('This test has not been implemented yet.');
     }
 
-    public function testNothing()
+    public function generalFilesProvider()
+    {
+
+    }
+
+    public function localeSpecificFilesProvider()
+    {
+
+    }
+
+    /**
+     * @param string $fileName
+     *
+     * @dataProvider generalFilesProvider
+     */
+    public function testLoadGeneralFiles($fileName)
+    {
+
+    }
+
+    /**
+     * @param string $locale
+     * @param string $fileName
+     *
+     * @dataProvider localeSpecificFilesProvider
+     */
+    public function testLoadLocaleSpecificFiles($locale, $fileName)
     {
 
     }
