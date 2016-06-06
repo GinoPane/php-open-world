314ac3920ddf2475ef328646f07ffac889742fb8
Gino Pane <sergey.karavay@gmail.com>
2016-06-06 15:11:21 +0300

    Add file list to dataTest.

--- tests/suites/data/dataTest.php
+++ tests/suites/data/dataTest.php
@@ -15,12 +15,32 @@ class DataTest extends PHPUnit_Framework_TestCase
 
     public function generalFilesProvider()
     {
-
+        return array(
+            'currency.codes.json',
+            'currency.fractions.json',
+            'language.alias.json',
+            'language.territories.json',
+            'likely.subtags.json',
+            'number.systems.json',
+            'territory.alias.json',
+            'territory.codes.json',
+            'territory.containment.json',
+            'territory.currencies.json',
+            'territory.info.json'
+        );
     }
 
     public function localeSpecificFilesProvider()
     {
-
+        return array(
+            'currency.names.json',
+            'identity.json',
+            'language.names.json',
+            'number.currencies.json',
+            'number.symbols.json',
+            'script.names.json',
+            'territory.names.json'
+        );
     }
 
     /**
