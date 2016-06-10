f3ffbd8a49504c3f5c022896b05c749b66f68444
Gino Pane <sergey.karavay@gmail.com>
2016-06-10 13:00:40 +0300

    Change required php version, minor updates.

--- tests/suites/data/dataTest.php
+++ tests/suites/data/dataTest.php
@@ -15,37 +15,74 @@ class DataTest extends PHPUnit_Framework_TestCase
 
     public function generalFilesProvider()
     {
-        return array(
-            'currency.codes.json',
-            'currency.fractions.json',
-            'language.alias.json',
-            'language.territories.json',
-            'likely.subtags.json',
-            'number.systems.json',
-            'territory.alias.json',
-            'territory.codes.json',
-            'territory.containment.json',
-            'territory.currencies.json',
-            'territory.info.json'
-        );
+        return [
+            [
+                'currency.codes.json'
+            ],
+            [
+                'currency.fractions.json'
+            ],
+            [
+                'language.alias.json'
+            ],
+            [
+                'language.territories.json'
+            ],
+            [
+                'likely.subtags.json'
+            ],
+            [
+                'number.systems.json'
+            ],
+            [
+                'territory.alias.json'
+            ],
+            [
+                'territory.codes.json'
+            ],
+            [
+                'territory.containment.json'
+            ],
+            [
+                'territory.currencies.json'
+            ],
+            [
+                'territory.info.json'
+            ]
+        ];
     }
 
     public function localeSpecificFilesProvider()
     {
-        return array(
-            'currency.names.json',
-            'identity.json',
-            'language.names.json',
-            'number.currencies.json',
-            'number.symbols.json',
-            'script.names.json',
-            'territory.names.json'
-        );
+        return [
+            [
+                'currency.names.json'
+            ],
+            [
+                'identity.json'
+            ],
+            [
+                'language.names.json'
+            ],
+            [
+                'number.currencies.json'
+            ],
+            [
+                'number.symbols.json'
+            ],
+            [
+                'script.names.json'
+            ],
+            [
+                'territory.names.json'
+            ]
+        ];
     }
 
     /**
      * @param string $fileName
      *
+     * @test
      * @dataProvider generalFilesProvider
      */
     public function testLoadGeneralFiles($fileName)
@@ -57,6 +94,7 @@ class DataTest extends PHPUnit_Framework_TestCase
      * @param string $locale
      * @param string $fileName
      *
+     * @test
      * @dataProvider localeSpecificFilesProvider
      */
     public function testLoadLocaleSpecificFiles($locale, $fileName)
