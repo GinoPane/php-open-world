9f776f4c8d29aff62e10ea55a1c4f948948b99e9
Gino Pane <sergey.karavay@gmail.com>
2016-07-05 23:46:13 +0300

    Remove files due to refactoring reasons.

--- src/data/dataFile.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace OpenWorld\Data;
-
-class DataFile {
-
-}
--- src/data/dataFileInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace OpenWorld\Data;
-
-interface DataFileInterface {
-
-    public function load($fileName = '', $condition = '');
-
-}
